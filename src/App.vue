6ae21a0ace1e32c2a3fa611d4471fe3c3725f9e1
Tetiana Baranova <syniovych.tanya@gmail.com>
2026-08-29 22:05:06 +0300

    add header component

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,21 @@
 <template>
-    <div v-if="num < 3">
-        <h1>Hello</h1>
-        <p>First</p>
-    </div>
-    <div v-else-if="num == 3">
-        <p>Vue забезпечує легке відстеження дій, зміни HTML структури та оновлення інформації без перезавантаження сторінки.</p>
-    </div>
-    <div v-else="">Last block</div>
-    <input @input="this.num = $event.target.value" />
-    <button @click="this.isVisible = !this.isVisible">Click</button>
-    <div v-if="isVisible">Hi</div>
-
-    <p>
-        {{ books[1].author }}
-    </p>
-
-    <ul>
-        <li v-for="(book, index) in books" v-bind:key="index">
-            {{ index +1 }} - {{ book.title }} - <b>{{ book.author }}</b> / <em>{{ book.genre }}</em>
-        </li>
-    </ul>
+    <Header title="Vue JS" />
 </template>
 
 <script>
+import Header from './components/Header.vue';
 export default {
-    data(){
-        return {
-            num: 5,
-            isVisible: false,
-            books: [
-                {
-                    title: "1984",
-                    author: "Джордж Орвелл",
-                    year: 1949,
-                    genre: "Антиутопія",
-                    isAvailable: true
-                },
-                {
-                    title: "Тигролови",
-                    author: "Іван Багряний",
-                    year: 1944,
-                    genre: "Пригодницький роман",
-                    isAvailable: false
-                },
-                {
-                    title: "Інтернат",
-                    author: "Сергій Жадан",
-                    year: 2017,
-                    genre: "Сучасна проза",
-                    isAvailable: true
-                }
-            ]
-        }
-
-    },
-    methods:{
-    },
-    computed:{
+    components:{
+        Header
     }
 
 };
 </script>
 <style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="header">
+        <span class="logo">{{ title }}</span>
+        <button @click="addRating">+</button>
+        <span class="user">{{ fullName }} ({{ user.rating }})</span>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            user:{
+                name: 'Anna',
+                surname: 'Doe',
+                age: 20,
+                rating: 0,
+                email: 'annadoe@gmail.com'
+            },
+            allRating: 0
+        };
+    },
+    computed: {
+        fullName(){
+            return `Hello  ${this.user.name} ${this.user.surname}`
+        },
+    },
+    methods:{
+        addRating(){
+            this.user.rating++;
+            this.allRating++;
+        },
+    },
+    mounted(){
+        this.addRating()
+    },
+    watch:{
+        allRating(newValue, oldValue){
+            if(newValue > 5)
+                console.log(`${newValue} - ${oldValue}`);
+        }
+    },
+    props:{
+        title:{
+            type: String,
+            required: true,
+        }
+    },
+
+
+};
+</script>
+<style lang="css" scoped>
+.header{
+    background-color: #a6534afe;
+    color: white;
+    padding: 20px;
+    border-bottom: 2px solid #443f3f;
+}
+
+.header .logo{
+    font-size: 20px;
+}
+.header .user{
+    float: right;
+}
+.header button{
+    float: right;
+    border: 0;
+    color: rgb(255, 255, 255);
+    background-color: #2f0303;
+    border-radius: 5px;
+    padding: 10px 15px;
+    font-size: 12px;
+    font-weight: bold;
+    cursor: pointer;
+    margin-left: 5px;
+    position: relative;
+    top: -5px;
+}
+.header button:hover{
+    background-color: #d08d8d;
+}
+</style>
