9e5888ac74b6e68dcda6625931de5844fea60ec5
Tetiana Baranova <syniovych.tanya@gmail.com>
2026-08-29 21:22:38 +0300

    practice

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,16 @@
     <input @input="this.num = $event.target.value" />
     <button @click="this.isVisible = !this.isVisible">Click</button>
     <div v-if="isVisible">Hi</div>
+
+    <p>
+        {{ books[1].author }}
+    </p>
+
+    <ul>
+        <li v-for="(book, index) in books" v-bind:key="index">
+            {{ index +1 }} - {{ book.title }} - <b>{{ book.author }}</b> / <em>{{ book.genre }}</em>
+        </li>
+    </ul>
 </template>
 
 <script>
@@ -18,6 +28,29 @@ export default {
         return {
             num: 5,
             isVisible: false,
+            books: [
+                {
+                    title: "1984",
+                    author: "Джордж Орвелл",
+                    year: 1949,
+                    genre: "Антиутопія",
+                    isAvailable: true
+                },
+                {
+                    title: "Тигролови",
+                    author: "Іван Багряний",
+                    year: 1944,
+                    genre: "Пригодницький роман",
+                    isAvailable: false
+                },
+                {
+                    title: "Інтернат",
+                    author: "Сергій Жадан",
+                    year: 2017,
+                    genre: "Сучасна проза",
+                    isAvailable: true
+                }
+            ]
         }
 
     },
